cdab223003ad3a2b5d2de362926bc080d32cec39
uginza <uginza@yandex.ru>
2023-03-26 15:19:32 +0300

    Skill.tsx and Skill.module.css are done

--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import s from './Skill.module.css';
 
+type SkillPropsType={
+    skillName:string
+    description:string
+}
 
-export function Skill() {
+export function Skill(props:SkillPropsType) {
     return (
-        <div className={s.skillBlock}>
             <div className={s.skill}>
-
+                <div className={s.icon}></div>
+                <h3>{props.skillName}</h3>
+                <span className={s.description}>{props.description}</span>
             </div>
-        </div>
     );
 }
