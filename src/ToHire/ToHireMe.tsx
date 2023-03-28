51cceb3d145873fe6480dfc371d4e3de52cb7717
uginza <uginza@yandex.ru>
2023-03-28 22:21:59 +0300

    Styles are fixed

--- src/ToHire/ToHireMe.tsx
+++ src/ToHire/ToHireMe.tsx
@@ -6,7 +6,7 @@ import containerStyle from '../common/styles/containerStyle.module.css'
 export function ToHireMe() {
     return (
         <div className={s.toHireBlock}>
-            <div className={`${containerStyle.container} ${s.myProjectsBlockContainer}`}>
+            <div className={`${containerStyle.container} ${s.toHireBlockContainer}`}>
                 <h2>Remote work offers is considered</h2>
                 <button>Hire me</button>
             </div>
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -9,9 +9,9 @@ export function Skills() {
             <div className={`${containerStyle.container} ${s.skillBlockContainer}`}>
                 <h2>Skills</h2>
                 <div className={s.skills}>
-                    <Skill skillName={"HTML/CSS"} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis, ipsum at aliquet varius'}/>
-                    <Skill skillName={'JS/TS'} description={'At vero eos et accusamus et iusto odio dignissimos'}/>
-                    <Skill skillName={'REACT/REDUX'} description={'placeat facere possimus, omnis voluptas assumenda est,'}/>
+                    <Skill skillName={"HTML /CSS"} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis, ipsum at aliquet varius'}/>
+                    <Skill skillName={'JS /TS'} description={'At vero eos et accusamus et iusto odio dignissimos'}/>
+                    <Skill skillName={'REACT /REDUX'} description={'placeat facere possimus, omnis voluptas assumenda est,'}/>
                 </div>
             </div>
         </div>
