a52948d3a68f26df6e33c988ed57579ed211574e
uginza <uginza@yandex.ru>
2023-05-16 12:17:36 +0300

    MyProject.module.css is in progress of colors and fonts 2

--- src/myProjects/MyProject/MyProject.tsx
+++ src/myProjects/MyProject/MyProject.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import s from './MyProject.module.css';
 
-type SkillPropsType={
-    projectName:string
-    description:string
+type MyProjectPropsType = {
+    projectName: string
+    description: string
 }
 
-export function MyProject(props:SkillPropsType) {
+export function MyProject(props: MyProjectPropsType) {
     return (
-            <div className={s.skill}>
+        <div className={s.myProject}>
+            <div className={s.image}>
                 <button className={s.button}>Show me</button>
-                <h3>{props.projectName}</h3>
+            </div>
+
+            <div className={s.myProjectInfo}>
+                <h3 className={s.myProjectTitle}>{props.projectName}</h3>
                 <span className={s.description}>{props.description}</span>
             </div>
+        </div>
     );
 }
