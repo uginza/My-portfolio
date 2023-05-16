55cb1db2f2d4ba4358d725c23c4e51ed53d8e408
uginza <uginza@yandex.ru>
2023-05-16 12:56:17 +0300

    Screens of projects are added to MyProjects.tsx

--- src/myProjects/MyProject/MyProject.tsx
+++ src/myProjects/MyProject/MyProject.tsx
@@ -4,15 +4,15 @@ import s from './MyProject.module.css';
 type MyProjectPropsType = {
     projectName: string
     description: string
+    style:any
 }
 
 export function MyProject(props: MyProjectPropsType) {
     return (
         <div className={s.myProject}>
-            <div className={s.image}>
+            <div className={s.image} style={props.style}>
                 <button className={s.button}>Show me</button>
             </div>
-
             <div className={s.myProjectInfo}>
                 <h3 className={s.myProjectTitle}>{props.projectName}</h3>
                 <span className={s.description}>{props.description}</span>
--- src/myProjects/MyProjects.tsx
+++ src/myProjects/MyProjects.tsx
@@ -3,15 +3,23 @@ import s from './MyProjects.module.css';
 import containerStyle from '../common/styles/containerStyle.module.css'
 import {MyProject} from "./MyProject/MyProject";
 import {Title} from "../common/components/title/Title";
+import todolistImg from '../assets/images/todolist.png'
+import socialNetworkImg from '../assets/images/social-network.jpg'
 
 export function MyProjects() {
+    const todolistImage = {
+        backgroundImage: `url(${todolistImg})`,
+    };
+    const socialNetworkImage = {
+        backgroundImage: `url(${socialNetworkImg})`,
+    };
     return (
         <div className={s.myProjectsBlock}>
             <div className={`${containerStyle.container} ${s.myProjectsBlockContainer}`}>
                 <Title text={'My projects'}/>
                 <div className={s.projects}>
-                    <MyProject projectName={'Social Network'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis, ipsum at aliquet varius'}/>
-                    <MyProject projectName={'Todolist'} description={'At vero eos et accusamus et iusto odio dignissimos'}/>
+                    <MyProject style={socialNetworkImage} projectName={'Social Network'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis, ipsum at aliquet varius'}/>
+                    <MyProject style={todolistImage} projectName={'Todolist'} description={'At vero eos et accusamus et iusto odio dignissimos'}/>
                 </div>
             </div>
         </div>
