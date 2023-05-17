b13b7f340bb35d69b81072fd355e72fd663171a3
uginza <uginza@yandex.ru>
2023-05-17 16:37:22 +0300

    MainPage.tsx MainPage.module.scss are done with colors and fonts

--- src/mainPage/MainPage.tsx
+++ src/mainPage/MainPage.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import s from './MainPage.module.css';
+import s from './MainPage.module.scss';
 import containerStyle from '../common/styles/containerStyle.module.css'
+import myPhoto from '../assets/images/myPhoto.jpg'
 
 
 export function MainPage() {
+    const myPhotoImage = {
+        backgroundImage: `url(${myPhoto})`,
+    };
     return (
         <div className={s.mainPage}>
-            <div className={containerStyle.container}>
-                <div className={s.text}>
-                    <span>Hi there!</span>
-                    <h1>I'm Evgeniy Zakharov</h1>
-                    <div>Frontend developer</div>
+            <div className={`${containerStyle.container} ${s.MainPageBlockContainer}`}>
+                <div className={s.mainPageInfo}>
+                    <h6 className={s.description}>Hi there!I'm Evgeniy Zakharov</h6>
+                    <h1 className={s.mainPageTitle}>
+                       Frontend developer</h1>
                 </div>
-                <div className={s.photo}></div>
+                <div className={s.photo} style={myPhotoImage}></div>
             </div>
 
         </div>
