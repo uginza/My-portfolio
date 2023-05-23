7fccb705db72c5d5026a0ebed76bd7a8ebf65459
uginza <uginza@yandex.ru>
2023-05-23 17:12:05 +0300

    smooth scroll is added;

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import s from './Nav.module.scss';
+import {Link} from 'react-scroll'
 
 export function Nav() {
     return (
         <div className={s.nav}>
-            <a className={s.text} href={"#projects"}>Main Page</a>
-            <a className={s.text} href={"#skills"}>Skills</a>
-            <a className={s.text} href={"projects"}>Projects</a>
-            <a className={s.text} href="https://www.google.com/">Contacts</a>
+            <a className={s.text} href=''> Main Page</a>
+            <Link className={s.text} activeClass={s.active} to="skills" spy={true} smooth={true} offset={0}
+                  duration={1500}>
+                Skills
+            </Link>
+            <Link className={s.text} activeClass={s.active} to="projects" spy={true} smooth={true} offset={0}
+                  duration={1500}>
+                Projects
+            </Link>
+            <Link className={s.text} activeClass={s.active} to="contacts" spy={true} smooth={true} offset={0}
+                  duration={1500}>
+                Contacts
+            </Link>
         </div>
     );
 }
