cfbe5880ba12ccaead8d2719f83dff55346717c3
uginza <uginza@yandex.ru>
2023-05-27 18:56:50 +0300

    header is over all pages during of scroll process

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -5,16 +5,19 @@ import {Link} from 'react-scroll'
 export function Nav() {
     return (
         <div className={s.nav}>
-            <a className={s.text} href=''> Main Page</a>
-            <Link className={s.text} activeClass={s.active} to="skills" spy={true} smooth={true} offset={0}
+            <Link className={s.text} activeClass={s.active} to="mainPage" spy={true} smooth={true} offset={-79}
+                  duration={1500}>
+                Main Page
+            </Link>
+            <Link className={s.text} activeClass={s.active} to="skills" spy={true} smooth={true} offset={-79}
                   duration={1500}>
                 Skills
             </Link>
-            <Link className={s.text} activeClass={s.active} to="projects" spy={true} smooth={true} offset={0}
+            <Link className={s.text} activeClass={s.active} to="projects" spy={true} smooth={true} offset={-79}
                   duration={1500}>
                 Projects
             </Link>
-            <Link className={s.text} activeClass={s.active} to="contacts" spy={true} smooth={true} offset={0}
+            <Link className={s.text} activeClass={s.active} to="contacts" spy={true} smooth={true} offset={-79}
                   duration={1500}>
                 Contacts
             </Link>
