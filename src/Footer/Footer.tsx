8f5b95bf79d0e2fb51518a87d1d012cd0eaa7aa7
uginza <uginza@yandex.ru>
2023-05-28 22:07:20 +0300

    Footer logos are added

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import s from './Footer.module.scss';
 import {Fade, Zoom} from "react-awesome-reveal";
+import telegramIcon from '../assets/images/telegramIcon.svg'
+import gitHubIcon from '../assets/images/github.svg'
+import linkedInIcon from '../assets/images/linkedin.svg'
+import instagramIcon from '../assets/images/instagram.svg'
 
 
 export function Footer() {
@@ -10,10 +14,10 @@ export function Footer() {
             <div className={s.FooterBlockContainer}>
                 <h2 className={s.text}>Evgeniy Zakharov</h2>
                 <div className={s.icons}>
-                    <div className={s.icon}></div>
-                    <div className={s.icon}></div>
-                    <div className={s.icon}></div>
-                    <div className={s.icon}></div>
+                    <img className={s.icon} src={telegramIcon} alt='telegramIcon'/>
+                    <img className={s.icon} src={gitHubIcon} alt='telegramIcon'/>
+                    <img className={s.icon} src={linkedInIcon} alt='telegramIcon'/>
+                    <img className={s.icon} src={instagramIcon} alt='telegramIcon'/>
                 </div>
                 <h3 className={s.description}>© 2023 all rights reserved</h3>
             </div>
